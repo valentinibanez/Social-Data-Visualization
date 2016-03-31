348104a2c64dc6754562c076a97e2dcb3b8d74eb
Jamie Neubert Pedersen <zajaka@hotmail.com>
2016-03-31 21:31:34 +0200

    Add Initial map visualization with points for D

--- d.js
+++ d.js
@@ -25,6 +25,82 @@ d3.json("https://raw.githubusercontent.com/suneman/socialdataanalysis2016/master
         .append("path")
         .attr("d", path);
 
+    var color = d3.scale.linear()
+        .domain([0, 1, 2])
+        .range(["red", "yellow", "green"]);
+    // Add the data
+    d3.csv("data/points.csv", function(data) {
+        dataset = data.map(function(d) { return [+d["x"], +d["y"], +d["label"]]; });
+
+        var xScale = d3.scale.linear()
+            .domain([
+                d3.min(dataset, function(d) { return d[0]; }),
+                d3.max(dataset, function(d) { return d[0]; })
+            ])
+            .range([20, 760]);
+
+        var yScale = d3.scale.linear()
+            .domain([
+                d3.min(dataset, function(d) { return d[1]; }),
+                d3.max(dataset, function(d) { return d[1]; })
+            ])
+            .range([760, 120]);
+
+
+        //Create circles
+        dVis.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) {
+                return xScale(d[0]);
+            })
+            .attr("cy", function(d) {
+                return yScale(d[1]);
+            })
+            .attr("r", function(d) {
+                return 5;
+            })
+            .style("fill", function(d) {
+                return color(d[2]);
+            });
+    });
+
+    // Add the center points
+    d3.csv("data/centroids.csv", function(data) {
+        dataset = data.map(function(d) { return [+d["x"], +d["y"]]; });
+
+        var xScale = d3.scale.linear()
+            .domain([
+                d3.min(dataset, function(d) { return d[0]; }),
+                d3.max(dataset, function(d) { return d[0]; })
+            ])
+            .range([150, 500]);
+
+        var yScale = d3.scale.linear()
+            .domain([
+                d3.min(dataset, function(d) { return d[1]; }),
+                d3.max(dataset, function(d) { return d[1]; })
+            ])
+            .range([500, 200]);
+
+
+        //Create circles
+        dVis.selectAll("circle")
+            .data(dataset)
+            .enter()
+            .append("circle")
+            .attr("cx", function(d) {
+                return xScale(d[0]);
+            })
+            .attr("cy", function(d) {
+                return yScale(d[1]);
+            })
+            .attr("r", function(d) {
+                return 24;
+            })
+            .style("fill", "rgba(208,110,228,0.7)");
+    });
 });
 var border = 1;
 var bordercolor = 'black';
@@ -36,3 +112,7 @@ var borderPath = dVis.append("rect")
     .style("stroke", bordercolor)
     .style("fill", "none")
     .style("stroke-width", border);
+
+
+
+
